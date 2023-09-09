b15b48d4b1d7bd6ea72d8df2f9d0eb0701eb069a
Shubham <144091679+thakur-shubham-singh-pathania@users.noreply.github.com>
2023-09-09 13:44:16 +0530

    Update streamlit_app.py
    Resolve the bug in the line 16

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,5 +16,5 @@ my_fruit_list = pandas.read.csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 # Let's put a pick list here so they can pick the fruit they want to include 
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 
-display the table on the page 
+# display the table on the page 
 streamlit.dataframe(my_fruit_list)
