0f0ccfb7edaef407ddf22743592d87f4a8e37d04
Shubham <144091679+thakur-shubham-singh-pathania@users.noreply.github.com>
2023-09-09 13:43:33 +0530

    Update streamlit_app.py
    Added the support for the data in the table using the s3 bucket url

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,3 +8,13 @@ streamlit.text('🥗 Kale,Spinach & Rocket smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+
+import pandas
+my_fruit_list = pandas.read.csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+# Let's put a pick list here so they can pick the fruit they want to include 
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+
+display the table on the page 
+streamlit.dataframe(my_fruit_list)
