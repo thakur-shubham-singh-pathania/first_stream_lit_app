2c2edf595288ed28b13e7388d22b3cc77c31ba3d
Shubham <144091679+thakur-shubham-singh-pathania@users.noreply.github.com>
2023-09-10 15:41:21 +0530

    Update streamlit_app.py
    Resolve the fruityvice error

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,3 +36,7 @@ streamlit.write('The user entered ', fruit_choice)
 
 import requests 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+
+# write your own comment - what does this do?
+streamlit.dataframe(fruityvice_normalized)
