dc9b8b0d12a343df4e83cf801b7776733aa9f3a7
Shubham <144091679+thakur-shubham-singh-pathania@users.noreply.github.com>
2023-09-10 15:42:27 +0530

    Update streamlit_app.py
    Remove the 2nd table

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,11 +26,6 @@ streamlit.header("Fruityvice Fruit Advice!")
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 
-# fruityvice table  
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# write your own comment - what does this do?
-streamlit.dataframe(fruityvice_normalized)
-
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
 
