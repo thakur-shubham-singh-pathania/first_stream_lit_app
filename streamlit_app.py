1eb81e34e69ed376b5a3798b72068eefda5278a2
Shubham <144091679+thakur-shubham-singh-pathania@users.noreply.github.com>
2023-09-12 14:47:05 +0530

    Update streamlit_app.py
    Resolve the error in the line 51

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,7 +48,7 @@ def get_fruit_load_list():
         my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
         return my_cur.fecthall()
 
-Add a button to load the fruit
+# Add a button to load the fruit
 if streamlit.button('Get Fruit Load List'):
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
     my_data_rows = get_fruit_load_list()
