083fe202815c61fb1d6cce0bb16922bfc91ba445
Shubham <144091679+thakur-shubham-singh-pathania@users.noreply.github.com>
2023-09-12 14:09:00 +0530

    Update streamlit_app.py
    Added support for insert query in streamlit

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,3 +47,5 @@ streamlit.dataframe(my_data_row)
 
 fruit_choice = streamlit.text_input('What fruit would you like to add ?','jackfruit')
 streamlit.write('Thanks for entering ', fruit_choice)
+
+my_cur.execute("insert into fruit_load_list values ('from streamlit')")
