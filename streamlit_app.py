c46801a0a19728dddceee510876b5687d2eb8b91
Shubham <144091679+thakur-shubham-singh-pathania@users.noreply.github.com>
2023-09-12 14:25:08 +0530

    Update streamlit_app.py
    Resolve identation error

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,10 +31,9 @@ try:
     streamlit.error("Please select a fruit to get information.")
   else:   
 # streamlit.write('The user entered ', fruit_choice)
-  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-  streamlit.dataframe(fruityvice_normalized)
-
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    streamlit.dataframe(fruityvice_normalized)
 except URLError as e:
   streamlit.error()
 
